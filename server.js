dcd3c52a669dc7812f500c16cb404b0dac2d97f9
chedi-debiche <ch.debiche@gmail.com>
2024-11-08 13:27:31 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// backend/server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connexion à la base de données MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.error('MongoDB connection failed:', error));
+
+// Définir les routes
+app.use('/api/auth', authRoutes);
+
+// Port d'écoute
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
